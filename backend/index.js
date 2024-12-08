56bd5f18cd69c42d3c9c773ba4a60b49421716b4
mdhassan-786 <ahrarul0341@gmail.com>
2024-12-09 02:13:51 +0530

    added backend file

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const dishes = require("./routes/dishRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://anantpatni1:r6ptkxcYj907vYFt@cluster0.dze4qzt.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+app.use("/api", dishes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
